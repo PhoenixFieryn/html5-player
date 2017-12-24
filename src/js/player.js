8844dbb8fb76640f297f317fd0fa525aa1f8e810
Austin Hou <austin@austinhou.com>
2017-12-24 13:09:45 -0800

    Fully working progress bar

--- src/js/player.js
+++ src/js/player.js
@@ -2,7 +2,6 @@ const player = document.querySelector('.player');
 const video = document.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -34,6 +33,11 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 //eventlistener for video to play/pause
 video.addEventListener('click', togglePlay);
 //eventlistener for video to update play/pause icon
@@ -46,4 +50,11 @@ toggle.addEventListener('click', togglePlay);
 //eventlistener for skip buttons to skip
 skipButtons.forEach(button => button.addEventListener('click', skip));
 //eventlistener for progress slider
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+//eventlistener for scrubbing throught the video on progress bar
+let mousedown = false;
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
